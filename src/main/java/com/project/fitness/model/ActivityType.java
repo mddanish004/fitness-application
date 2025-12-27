a588b8c6db3caaa8d19eb07c8dc2d234ab8a1911
Md Danish Ansari <m.danishansari004@gmail.com>
2025-12-27 18:17:19 +0530

    Designed the configured the Database structure and relationships

---
+++ src/main/java/com/project/fitness/model/ActivityType.java
@@ -0,0 +1,14 @@
+package com.project.fitness.model;
+
+public enum ActivityType {
+    RUNNING,
+    CYCLING,
+    SWIMMING,
+    WALKING,
+    STRENGTH_TRAINING,
+    HIIT,
+    DANCE,
+    PILATES,
+    ROWING,
+    OTHER
+}
